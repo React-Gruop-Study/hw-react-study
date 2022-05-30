dff9d7430f4d7ecef617d9850899be84fd86abdd
hw <gusdnd09@gmail.com>
2022-05-30 14:22:53 +0900

    wip

--- pages/index.js
+++ pages/index.js
@@ -8,72 +8,72 @@ let stockContext = React.createContext();
 
 export default function App() {
 
-  	let [shoes, setShoes] = useState(shoeData);
+	let [shoes, setShoes] = useState(shoeData);
 	let [stock, setStock] = useState([10, 11, 12]);
 
 	const SendQuery = (shoe) => {
 		Router.push({
-		pathname: '/Detail',
-		query: { shoe: JSON.stringify(shoe), stock: stock, setStock: setStock },
-		}, '/Detail/'+shoe.id);
-	}; 
-	
+			pathname: '/Detail',
+			query: { shoe: JSON.stringify(shoe), stock: stock, setStock: setStock },
+		}, '/Detail/' + shoe.id);
+	};
+
 	return (
-		<div className="App" style={{textAlign: 'center'}}>
+		<div className="App" style={{ textAlign: 'center' }}>
 
 			<div className={styles.background}>
 				<h1>20% Season Off</h1>
 			</div>
 			<div className='container'>
-				<stockContext.Provider value={ stock }>
+				<stockContext.Provider value={stock}>
 					<div className='row'>
-					{
-						shoes.map((shoe, i)=>{
-							return <SheosComponent shoe={ shoe } i={ i } key={ i }/>
-						})
-					}
+						{
+							shoes.map((shoe, i) => {
+								return <SheosComponent shoe={shoe} i={i} key={i} />
+							})
+						}
 					</div>
 				</stockContext.Provider>
 			</div>
-			<button className="btn btn-primary" onClick={ ()=>{
-				
-				axios.get('https://codingapple1.github.io/shop/data2.json')
-				.then((result)=>{ 
-					// let newArray = [...shoes];
-					// result.data.forEach((item, i) => {
-					// 	newArray.push(item);
-					// });
-					// setShoes(newArray);
-					setShoes([...shoes, ...result.data]);
-				 })
-				.catch(()=>{  
-					console.log('실패');
-				});
+			<button className="btn btn-primary" onClick={() => {
 
-			} }>더보기</button>
+				axios.get('https://codingapple1.github.io/shop/data2.json')
+					.then((result) => {
+						// let newArray = [...shoes];
+						// result.data.forEach((item, i) => {
+						// 	newArray.push(item);
+						// });
+						// setShoes(newArray);
+						setShoes([...shoes, ...result.data]);
+					})
+					.catch(() => {
+						console.log('실패');
+					});
+
+			}}>더보기</button>
 		</div>
 	)
-	
-	function SheosComponent (props) {
+
+	function SheosComponent(props) {
 
 		let stock = useContext(stockContext);
 
-		return	(
-			<div className="col-md-4" onClick={ ()=>{SendQuery(props.shoe)} }>
-				<img src={'https://codingapple1.github.io/shop/shoes'+ (props.i + 1) +'.jpg'} 
+		return (
+			<div className="col-md-4" onClick={() => { SendQuery(props.shoe) }}>
+				<img src={'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg'}
 					width="100%"
 				/>
-				<h4>{ props.shoe.title }</h4>
-				<p>{ props.shoe.content }</p>
-				<Test i={props.i}/>
+				<h4>{props.shoe.title}</h4>
+				<p>{props.shoe.content}</p>
+				<Test i={props.i} />
 			</div>
 		)
 	}
 
-	function Test(props){
+	function Test(props) {
 		let stock = useContext(stockContext);
 		return (
-			<p>{ stock[props.i] }</p>
+			<p>{stock[props.i]}</p>
 		)
 	}
 }
